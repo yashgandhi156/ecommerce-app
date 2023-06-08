124c250ec2535b6cdd7ef55869b2613e069126aa
yash <yashgandhi156@gmail.com>
2023-06-08 14:48:23 +0530

    setting up mongodb

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", false);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Error in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
+// module.exports = connectDB;
