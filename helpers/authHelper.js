ade5ef8d3f5b3e2480dfb2eb92af3cbe1d147674
yash <yashgandhi156@gmail.com>
2023-06-09 11:11:17 +0530

    login API

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 export const hashPassword = async (password) => {
   try {
     const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltPassword);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     return hashedPassword;
   } catch (error) {
     console.log(error);
