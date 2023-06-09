359ec0c7d08a5046248c7d01119f9eb13616837e
yash <yashgandhi156@gmail.com>
2023-06-09 11:38:36 +0530

    Protected Middlewares

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,10 @@ import express from "express";
 import {
   registerController,
   loginController,
+  testController,
 } from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
 //router object
 const router = express.Router();
 
@@ -12,4 +15,8 @@ router.post("/register", registerController);
 
 //LOGIN || POST
 router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
 export default router;
