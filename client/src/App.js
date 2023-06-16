0c96a3c0d98389aafaffbc89528c1ffb4b1c5694
yash <yashgandhi156@gmail.com>
2023-06-16 17:56:09 +0530

    search filter and single product similar product page

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ import Orders from "./pages/user/Order";
 import Profile from "./pages/user/Profile";
 import Products from "./pages/Admin/Products";
 import UpdateProduct from "./pages/Admin/UpdateProduct";
+import Search from "./pages/Search";
+import ProductDetails from "./pages/ProductDetails";
 // import { ToastContainer } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
 
@@ -26,6 +28,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
           <Route path="user/orders" element={<Orders />} />
